002a1ead3edd81e9a20a2aaa9ccfc2c63b0da913
JialeWangXu <jiale.wang@alumnos.upm.es>
2025-04-01 00:28:45 +0200

    changed status

--- src/app/api/company-jobs/add-job/route.ts
+++ src/app/api/company-jobs/add-job/route.ts
@@ -69,7 +69,7 @@ export async function POST(request:NextRequest){
         }catch(tokenError){
             return NextResponse.json({
                 error:message.error.noToken,tokenError
-            },{status:400})            
+            },{status:401})            
         }
 
     }catch(e){
