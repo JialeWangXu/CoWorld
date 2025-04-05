5e1ca1e9f570159179538e6082bd2f068b2f806e
JialeWangXu <jiale.wang@alumnos.upm.es>
2025-04-05 20:33:29 +0200

    added recomendation algorithm for demonstrating job list

--- src/app/api/candidate-home/get-jobs/route.ts
+++ src/app/api/candidate-home/get-jobs/route.ts
@@ -4,6 +4,7 @@ import Job from "models/Job";
 import { NextRequest, NextResponse } from "next/server";
 import { IJobAndCompany, JobFilters } from "types/JobFilter";
 import { message } from "util/message";
+import CandidateProfile from "models/CandidateProfile";
 
 export async function GET(request:NextRequest) {
     
@@ -71,6 +72,9 @@ export async function GET(request:NextRequest) {
                 const accessToken = request.cookies.get('accessTokenCookie').value;
                  //@ts-ignore
                 const {data} = jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET)
+
+                const profile = await CandidateProfile.findOne({user_id: data._id});
+                
                 console.log("Access aprobado para editar")
                 // obtener los filtros en forma de lista de key value
                 try{
@@ -78,21 +82,142 @@ export async function GET(request:NextRequest) {
                     console.log("Params:"+JSON.stringify(params))
                     const filters = filterParser(params);
                     
-                    console.log("Filtero: "+JSON.stringify(filters))
                     const jobQuery =query(filters);
-                    console.log("QUERY: "+JSON.stringify(jobQuery));
 
-                    //const jobPrueba = await Job.find({intership:false});
                     const job = await Job.aggregate([
                         {$match:{currentStatus:"active"}},
                         {$match:jobQuery},
+                        {
+                            $addFields:{
+                                cityMatch:{
+                                    $cond:[{$eq:["$city",profile.city]},1,0]
+                                },
+                                disabilitiesMatch:{
+                                    $reduce:{
+                                        input:"$disabilities",
+                                        initialValue:0,
+                                        in:{
+                                            $let:{
+                                                vars:{
+                                                    userDisability:{
+                                                        $arrayElemAt:[ // necesitamos esto, porque filter nos devuelve simrpe un array 
+                                                        {
+                                                            $filter:{
+                                                                input:profile.disabilities,
+                                                                as:"userDis",
+                                                                cond:{
+                                                                    $eq:["$$userDis.type","$$this.type"]
+                                                                }
+                                                            }
+                                                        },0]
+                                                    },
+                                                },
+                                            in:{
+                                                $add:["$$value",{                                                
+                                                    $cond:[ {$or: [
+                                                    { $eq: ["$$this.degree",-1] }, // Si la empresa no impone restricciones
+                                                    { $gte: ["$$this.degree","$$userDisability.degree"] } // Si el trabajo acepta el grado del candidato o uno mayor
+                                                ]},1,0]}] // solo add point si en etos casos
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                desireJobMatch:{
+                                    $size:{
+                                        $filter:{
+                                            input:profile.desiredJob,
+                                            as:"desiredJob",
+                                            cond:{
+                                                $or:[ { 
+                                                    $regexMatch: {
+                                                    input: "$jobTitle",
+                                                    regex: "$$desiredJob",
+                                                    options: "i"
+                                                    }
+                                                },              
+                                                { 
+                                                    $regexMatch: { 
+                                                    input: "$description",
+                                                    regex: "$$desiredJob",
+                                                    options: "i"
+                                                    }
+                                                },
+                                                { 
+                                                    $regexMatch: { 
+                                                    input: "$companysRequirements",
+                                                    regex: "$$desiredJob",
+                                                    options: "i"
+                                                    }
+                                                },{
+                                                    $in:[
+                                                        "$$desiredJob",
+                                                        "$requiredKnowledge"
+                                                    ]
+                                                }]
+                                            }
+                                        }
+
+                                    }
+                                },
+                                skillsMatch:{
+                                    $size:{
+                                        $filter:{
+                                            input:profile.skills,
+                                            as:"skill",
+                                            cond:{
+                                                $or:[ { 
+                                                    $regexMatch: {
+                                                    input: "$jobTitle",
+                                                    regex: "$$skill",
+                                                    options: "i"
+                                                    }
+                                                },              
+                                                { 
+                                                    $regexMatch: { 
+                                                    input: "$description",
+                                                    regex: "$$skill",
+                                                    options: "i"
+                                                    }
+                                                },
+                                                { 
+                                                    $regexMatch: { 
+                                                    input: "$companysRequirements",
+                                                    regex: "$$skill",
+                                                    options: "i"
+                                                    }
+                                                },{
+                                                    $in:[
+                                                        "$$skill",
+                                                        "$requiredKnowledge"
+                                                    ]
+                                                }]
+                                            }
+                                        }
+
+                                    }
+                                },
+                            } 
+                        },
+                        {$addFields:{
+                            finalScore:{
+                                $add:[
+                                    {$multiply:["$cityMatch",0.1]},
+                                    {$multiply:["$disabilitiesMatch",0.4]},
+                                    {$multiply:["$desireJobMatch",0.3]},
+                                    {$multiply:["$skillsMatch",0.2]}
+                                ]
+                            }
+                        }},
                         {$lookup:{
                             from:"companyprofiles",
                             localField:"company_id",
                             foreignField:"company_id",
                             as:"companyProfile"
                         }},
-                        {$unwind:{path:"$companyProfile",preserveNullAndEmptyArrays: true}}
+                        {$unwind:{path:"$companyProfile",preserveNullAndEmptyArrays: true}},
+                        {$sort:{
+                            finalScore:-1}}
                     ])
 
                     
--- src/app/api/candidate-home/search-jobs/route.ts
+++ src/app/api/candidate-home/search-jobs/route.ts
@@ -5,6 +5,7 @@ import Job from "models/Job";
 import { NextRequest, NextResponse } from "next/server";
 import { IJobAndCompany} from "types/JobFilter";
 import { message } from "util/message";
+import CandidateProfile from "models/CandidateProfile";
 
 export  async function POST(request:NextRequest){
 
@@ -21,7 +22,7 @@ export  async function POST(request:NextRequest){
                 const body = await request.json();
 
                 const {search} = body
-
+                const profile = await CandidateProfile.findOne({user_id: data._id});
                 const job= await Job.aggregate([
                     {$match:{
                         $text:{
@@ -32,6 +33,128 @@ export  async function POST(request:NextRequest){
                     {$addFields:{
                         score:{$meta:'searchScore'}
                     }},
+                    {
+                        $addFields:{
+                            cityMatch:{
+                                $cond:[{$eq:["$city",profile.city]},1,0]
+                            },
+                            disabilitiesMatch:{
+                                $reduce:{
+                                    input:"$disabilities",
+                                    initialValue:0,
+                                    in:{
+                                        $let:{
+                                            vars:{
+                                                userDisability:{
+                                                    $arrayElemAt:[ // necesitamos esto, porque filter nos devuelve simrpe un array 
+                                                    {
+                                                        $filter:{
+                                                            input:profile.disabilities,
+                                                            as:"userDis",
+                                                            cond:{
+                                                                $eq:["$$userDis.type","$$this.type"]
+                                                            }
+                                                        }
+                                                    },0]
+                                                },
+                                            },
+                                        in:{
+                                            $add:["$$value",{                                                
+                                                $cond:[ {$or: [
+                                                { $eq: ["$$this.degree",-1] }, // Si la empresa no impone restricciones
+                                                { $gte: ["$$this.degree","$$userDisability.degree"] } // Si el trabajo acepta el grado del candidato o uno mayor
+                                            ]},1,0]}] // solo add point si en etos casos
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            desireJobMatch:{
+                                $size:{
+                                    $filter:{
+                                        input:profile.desiredJob,
+                                        as:"desiredJob",
+                                        cond:{
+                                            $or:[ { 
+                                                $regexMatch: {
+                                                input: "$jobTitle",
+                                                regex: "$$desiredJob",
+                                                options: "i"
+                                                }
+                                            },              
+                                            { 
+                                                $regexMatch: { 
+                                                input: "$description",
+                                                regex: "$$desiredJob",
+                                                options: "i"
+                                                }
+                                            },
+                                            { 
+                                                $regexMatch: { 
+                                                input: "$companysRequirements",
+                                                regex: "$$desiredJob",
+                                                options: "i"
+                                                }
+                                            },{
+                                                $in:[
+                                                    "$$desiredJob",
+                                                    "$requiredKnowledge"
+                                                ]
+                                            }]
+                                        }
+                                    }
+
+                                }
+                            },
+                            skillsMatch:{
+                                $size:{
+                                    $filter:{
+                                        input:profile.skills,
+                                        as:"skill",
+                                        cond:{
+                                            $or:[ { 
+                                                $regexMatch: {
+                                                input: "$jobTitle",
+                                                regex: "$$skill",
+                                                options: "i"
+                                                }
+                                            },              
+                                            { 
+                                                $regexMatch: { 
+                                                input: "$description",
+                                                regex: "$$skill",
+                                                options: "i"
+                                                }
+                                            },
+                                            { 
+                                                $regexMatch: { 
+                                                input: "$companysRequirements",
+                                                regex: "$$skill",
+                                                options: "i"
+                                                }
+                                            },{
+                                                $in:[
+                                                    "$$skill",
+                                                    "$requiredKnowledge"
+                                                ]
+                                            }]
+                                        }
+                                    }
+
+                                }
+                            },
+                        } 
+                    },
+                    {$addFields:{
+                        finalScore:{
+                            $add:[
+                                {$multiply:["$cityMatch",0.1]},
+                                {$multiply:["$disabilitiesMatch",0.4]},
+                                {$multiply:["$desireJobMatch",0.3]},
+                                {$multiply:["$skillsMatch",0.2]}
+                            ]
+                        }
+                    }},
                     {$lookup:{
                         from:"companyprofiles",
                         localField:"company_id",
@@ -40,8 +163,9 @@ export  async function POST(request:NextRequest){
                     }},
                     {$unwind:{path:"$companyProfile",preserveNullAndEmptyArrays: true}},
                     {$sort:{
-                        createdAt: -1,
-                        score:-1
+                        finalScore:-1,
+                        score:-1,
+                        createdAt: -1
                     }}
                 ])
 
